fa653b74ca75e0754fa9007c4b1fb4f8f80ff0f6
thinktank-dev1 <wilson@thinktank.co.za>
2025-09-01 12:25:37 +0200

    Dev

--- resources/views/livewire/account/partials/header.blade.php
+++ resources/views/livewire/account/partials/header.blade.php
@@ -1,10 +1,14 @@
 <header class="topbar">
     <nav class="navbar top-navbar navbar-expand-md navbar-dark">
         <div class="navbar-header">
-            <a class="navbar-brand" href="index.html">
-                <b>
+            <a class="navbar-brand" href="{{ url('dashboard') }}">
+                <b class="d-none d-md-block">
                     <img src="{{ asset('img/logo.png') }}" alt="homepage" class="dark-logo" style="width: 160px;" />
                     <img src="{{ asset('img/logo-light.png') }}" alt="homepage" class="light-logo" style="width: 160px;" />
+                </b>
+                <b class="d-md-none">
+                    <img src="{{ asset('img/logo-sm.png') }}" alt="homepage" class="dark-logo" style="width: 60px;" />
+                    <img src="{{ asset('img/logo-sm.png') }}" alt="homepage" class="light-logo" style="width: 60px;"/>
                 </b> 
             </a>
         </div>
