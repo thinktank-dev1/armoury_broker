86d5953fa96d4b38f9e1023f8fe415441fada1ea
thinktank-dev1 <wilson@thinktank.co.za>
2025-10-09 15:49:37 +0200

    Dev

--- resources/views/livewire/account/dashboard.blade.php
+++ resources/views/livewire/account/dashboard.blade.php
@@ -245,7 +245,7 @@
                             </span>
                         </div>
                     </div>
-                    <div class="row mt-3">
+                    <div class="row">
                         <div class="col-12 col-md-8 offset-md-2">
                             <div class="order-grid mt-2">
                                 <a href="" class="order-box">
