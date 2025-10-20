d6e8d377ace839f06f6417d159795d883e13203c
thinktank-dev1 <wilson@thinktank.co.za>
2025-10-20 10:48:23 +0200

    Dev

--- resources/views/components/layouts/landing.blade.php
+++ resources/views/components/layouts/landing.blade.php
@@ -51,10 +51,6 @@
         <div class="modal fade" tabindex="-1" id="logout-warning">
             <div class="modal-dialog">
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
                     <div class="modal-body">
                         <h2 class="text-lg font-semibold mb-2">Session Expiring Soon</h2>
                         <p>You will be logged out in <span id="countdown">30</span> seconds due to inactivity.</p>
