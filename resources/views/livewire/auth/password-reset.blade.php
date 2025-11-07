ba485972713b39acd9cf4433a6bd3b486b65f42e
thinktank-dev1 <wilson@thinktank.co.za>
2025-11-07 14:24:33 +0200

    Dev

--- resources/views/livewire/auth/password-reset.blade.php
+++ resources/views/livewire/auth/password-reset.blade.php
@@ -5,12 +5,14 @@
                 <div class="col-md-6 offset-md-3">
                     <div class="card auth-cont">
                         <div class="card-body px-4">
+                            {{--
                             <div class="d-grid mt-3">
                                 <div class="btn-group">
                                     <a href="{{ url('auth/login') }}" class="btn btn-auth">Login</a>
                                     <a href="{{ url('auth/register') }}" class="btn btn-auth">Register</a>
                                 </div>
                             </div>
+                            --}}
                             <div class="row">
                                 <div class="col-md-12 text-center mt-4">
                                     <h4 class="page-title">Reset Password</h4>
@@ -39,7 +41,10 @@
                                     <input type="email" class="form-control" placeholder="Email" name="email" wire:model.defer="email">
                                 </div>
                                 <div class="mb-4">
-                                    <input type="password" class="form-control" placeholder="New Password" name="password" wire:model.defer="password">
+                                    <div class="password-wrapper">
+                                        <input type="password" class="form-control" placeholder="New Password" id="password" name="password" wire:model.defer="password">
+                                        <span id="togglePassword" class="toggle-icon"><i class="fa fa-eye"></i></span>
+                                    </div>
                                 </div>
                                 <div class="row mt-3 mb-5">
                                     <div class="col-md-12 text-center">
@@ -53,4 +58,16 @@
             </div>
         </div>
     </div>
+    @push('scripts')
+    <script>
+        const passwordInput = document.getElementById("password");
+        const togglePassword = document.getElementById("togglePassword");
+
+        togglePassword.addEventListener("click", () => {
+            const isPassword = passwordInput.type === "password";
+            passwordInput.type = isPassword ? "text" : "password";
+            togglePassword.innerHTML = isPassword ? "<i class='fa fa-eye-slash'></i>" : "<i class='fa fa-eye'></i>";
+        });
+    </script>
+    @endpush
 </div>
