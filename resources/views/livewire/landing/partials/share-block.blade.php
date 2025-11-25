3c7c8bf7a26d1f71602e23542b509ca8b0ba2918
thinktank-dev1 <wilson@thinktank.co.za>
2025-11-25 13:44:57 +0200

    Dev

--- resources/views/livewire/landing/partials/share-block.blade.php
+++ resources/views/livewire/landing/partials/share-block.blade.php
@@ -122,8 +122,8 @@
     <div class="modal fade" tabindex="-1" id="message-modal" wire:ignore.self>
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Contact {{ $vendor->name }}</h5>
+                <div class="modal-header bg-blue">
+                    <h5 class="modal-title text-white">Contact {{ $vendor->name }}</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
