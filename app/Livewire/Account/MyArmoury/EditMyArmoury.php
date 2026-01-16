d61e34c2b4b947de5e9c341abda428ef0c72c56e
thinktank-dev1 <wilson@thinktank.co.za>
2026-01-16 16:07:03 +0200

    Dev

--- app/Livewire/Account/MyArmoury/EditMyArmoury.php
+++ app/Livewire/Account/MyArmoury/EditMyArmoury.php
@@ -101,9 +101,9 @@ class EditMyArmoury extends Component
             Message::create([
                 'message_thread_id' => $tr->id,
                 'user_id' => 1,
-                'message' => "Hi ".Auth::user()->name."\n,
-                Thank you for joining our trusted community. You're now part of South Africa's leading secure marketplace transforming how firearms owners buy, sell, and trade equipment in a safe and secure environment.\n\n
-                You're currently viewing your ecommerce store dashboard—your central hub for accessing all platform features. The Dashboard tab in the left-hand menu provides an overview of your store with convenient shortcut links to navigate the entire platform. Getting around is simple: use either the left-hand menu tabs or the dashboard shortcuts.\n\n
+                'message' => "Hi ".Auth::user()->name."<br />,
+                Thank you for joining our trusted community. You're now part of South Africa's leading secure marketplace transforming how firearms owners buy, sell, and trade equipment in a safe and secure environment.<br /><br />
+                You're currently viewing your ecommerce store dashboard—your central hub for accessing all platform features. The Dashboard tab in the left-hand menu provides an overview of your store with convenient shortcut links to navigate the entire platform. Getting around is simple: use either the left-hand menu tabs or the dashboard shortcuts.<br /><br />
                 Ready to start? Browse quality equipment from verified sellers, list your unused gear, or explore competitive prices with complete peace of mind.
                 ",
                 'read_status' => 0,
