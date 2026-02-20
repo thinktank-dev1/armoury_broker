bfda81e0e45c755ee955bf4775fbbd06787f8c8f
thinktank-dev1 <wilson@thinktank.co.za>
2026-02-20 11:18:07 +0200

    Dev

--- database/seeders/AdminAccountSeeder.php
+++ database/seeders/AdminAccountSeeder.php
@@ -25,42 +25,45 @@ class AdminAccountSeeder extends Seeder
     }
 
     public function init(){
-        $sourcePath = database_path('seeders/assets/admin.png');
-        $fileName = 'admin_' . Str::random(10) . '.png';
-        $destinationPath = 'vendor_avater/' . $fileName;
+        $usr = User::where('email', 'admin@thinktank.co.za')->first();
+        if(!$usr){
+            $sourcePath = database_path('seeders/assets/admin.png');
+            $fileName = 'admin_' . Str::random(10) . '.png';
+            $destinationPath = 'vendor_avater/' . $fileName;
 
-        Storage::disk('public')->put(
-            $destinationPath,
-            file_get_contents($sourcePath)
-        );
+            Storage::disk('public')->put(
+                $destinationPath,
+                file_get_contents($sourcePath)
+            );
 
-        $rl = Role::where('name', 'admin')->first();
+            $rl = Role::where('name', 'admin')->first();
 
-        $vnd = Vendor::create([
-            'name' => 'Amoury Broker',
-            'url_name' => 'amoury-broker',
-            'avatar' => $destinationPath,
-            // 'banner' => null,
-            'description' => 'Aliqua tempor laboris deserunt id nulla eiusmod occaecat in irure. Dolore laborum enim commodo deserunt ex voluptate aliqua anim deserunt amet deserunt culpa.',
-            'suburb' => 'Centurion',
-            'city' => 'Pretoria',
-            'province' => 'Gauteng',
-            'instagram_handle' => '@armourybroker',
-            'status' => 1,
-        ]);
+            $vnd = Vendor::create([
+                'name' => 'Amoury Broker',
+                'url_name' => 'amoury-broker',
+                'avatar' => $destinationPath,
+                // 'banner' => null,
+                'description' => 'Aliqua tempor laboris deserunt id nulla eiusmod occaecat in irure. Dolore laborum enim commodo deserunt ex voluptate aliqua anim deserunt amet deserunt culpa.',
+                'suburb' => 'Centurion',
+                'city' => 'Pretoria',
+                'province' => 'Gauteng',
+                'instagram_handle' => '@armourybroker',
+                'status' => 1,
+            ]);
 
-        $usr = User::create([
-            'role_id' => $rl->id,
-            'vendor_id' => $vnd->id,
-            'name' => 'Amoury',
-            'surname' => 'Broker',
-            'mobile_number' => '0987654321',
-            'email' => 'admin@thinktank.co.za',
-            'password' => Hash::make('test1234'),
-            'status' => 1,
-            'avatar' => $destinationPath,
-            'email_verified_at' => now(),
-        ]);
-        $usr->markEmailAsVerified();
+            $usr = User::create([
+                'role_id' => $rl->id,
+                'vendor_id' => $vnd->id,
+                'name' => 'Amoury',
+                'surname' => 'Broker',
+                'mobile_number' => '0987654321',
+                'email' => 'admin@thinktank.co.za',
+                'password' => Hash::make('test1234'),
+                'status' => 1,
+                'avatar' => $destinationPath,
+                'email_verified_at' => now(),
+            ]);
+            $usr->markEmailAsVerified();
+        }
     }
 }
