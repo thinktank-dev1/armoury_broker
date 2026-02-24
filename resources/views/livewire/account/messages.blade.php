b9da6768553b168571b622360d5cc3bd519a407a
thinktank-dev1 <wilson@thinktank.co.za>
2026-02-24 09:34:22 +0200

    Dev

--- resources/views/livewire/account/messages.blade.php
+++ resources/views/livewire/account/messages.blade.php
@@ -362,8 +362,10 @@
                                 <li><strong>Payments outside Armoury Broker</strong> violate our Terms and Conditions and are not protected.</li>
                                 <li><strong>Never open external links or scan QR codes</strong> sent through platform messaging. Keep all communication on the platform.</li>
                                 <li><strong>Armoury Broker will never ask you to change</strong> your login information, banking details, or social media account information.</li>
+                                {{--
                                 <li>All firearm transactions <strong>must comply with the Firearms Control Act of South Africa 60 of 2000.</strong></li>
                                 <li>Legal liability for correct firearm transfer procedures <strong>rests with the license holder</strong> and not Armoury Broker (Pty) Ltd or any of its members.</li>
+                                --}}
                             </ul>
                         </div>
                         <div class="modal-footer">
