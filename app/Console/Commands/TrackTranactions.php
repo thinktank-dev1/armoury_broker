97989e6851d50b356a0a847424eddaf1d55ca36e
thinktank-dev1 <wilson@thinktank.co.za>
2026-07-06 14:57:29 +0200

    Dev

--- app/Console/Commands/TrackTranactions.php
+++ app/Console/Commands/TrackTranactions.php
@@ -174,7 +174,7 @@ class TrackTranactions extends Command
                 }
 
                 $order->g_payment_id = $res['id'];
-                $order->amount_paid = $paid_amount;
+                $order->amount_paid = $res['amount']/100;
                 $order->status = 'COMPLETE';
                 $order->save();
 
