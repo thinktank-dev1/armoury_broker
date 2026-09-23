0f8e411464505220a5b6e4218f73394198af43ba
thinktank-dev1 <wilson@thinktank.co.za>
2026-09-23 10:02:10 +0200

    Dev

--- app/Livewire/Landing/PaymentNotice.php
+++ app/Livewire/Landing/PaymentNotice.php
@@ -5,6 +5,7 @@ namespace App\Livewire\Landing;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 
+use Log;
 use App\Models\Order;
 use App\Models\OrderItem;
 
@@ -24,7 +25,9 @@ class PaymentNotice extends Component
         $order_no = Request::input('reference');
 
         $id = (int)preg_replace('/[^0-9]/', '', $order_no);
-        $order = Order::find($id); 
+        $order = Order::find($id);
+
+        Log::info(Request::all()); 
 
         return view('livewire.landing.payment-notice', [
             'status' => $status,
